d1f4f8609a967efd5eecbc5b48bcf55483508413
kamboj-s36 <kamboj-s36@142.132.137.68>
2018-11-23 10:31:43 -0600

    Array exersie Question) 3
    -asks to enter array
    -prints actual array length
    -asks to enter desired array length
    -asks for index
    -prints out value for that index
    - if index is greater that array length then print message
    - if index is less than 0 then program ends with message

--- src/array1/Array1.java
+++ src/array1/Array1.java
@@ -15,40 +15,67 @@ public class Array1 {
 
    
     public static void main(String[] args) {
+        int index = 0;
+        int arrayLength;
+        int[] intArray; 
+        int arrayActualLength;
+        int y;
+        String stringIndex;
+        
         
         System.out.println("Enter array. Use spacebar to seperate numbers:");
         Scanner scanInput = new Scanner(System.in);
         String input = scanInput.nextLine();
         
-        System.out.println("Enter the  length of array: ");
-        String stringLengthInput = scanInput.nextLine();
-        int arrayLength = Integer.parseInt(stringLengthInput);
         //System.out.println("Array length =" + arrayLength);
         String[] stringArray = input.split(" ");
-       // int arrayLength = stringArray.length;
-        int[] intArray = new int[stringArray.length]; 
-        int y = arrayLength;
+        arrayActualLength = stringArray.length;
+        intArray = new int[stringArray.length]; 
+        
+        System.out.println("Actual length =  " + arrayActualLength );
+        System.out.println("");
+        System.out.println("Enter the length of array: ");
+        String stringLengthInput = scanInput.nextLine();
+        arrayLength = Integer.parseInt(stringLengthInput);
+        //y = arrayLength;
+        
         for(int i = 0;i < stringArray.length;i++){
             intArray[i] = Integer.parseInt(stringArray[i]);
-        }
+        }// end for
         
         //printArray(intArray,arrayLength);
-        System.out.println("Original array");
-        for(int i = 0; i < arrayLength;i++ ){
-            printArray(intArray,arrayLength,i);
-            }
+//        System.out.println("Original array");
+//        for(int i = 0; i < arrayLength;i++ ){
+//            printArray(intArray,arrayLength,i);
+//            }
+//        
+//        System.out.println(" ");
+//        System.out.println("Reverse array");
+//        for(int r = arrayLength; r > 0;r-- ){
+//            //System.out.println("R = " + r);
+//            printArray(intArray,arrayLength,r-1);
+//            }
         
-        System.out.println(" ");
-        System.out.println("Reverse array");
-        for(int r = arrayLength; r > 0;r-- ){
-            //System.out.println("R = " + r);
-            printArray(intArray,arrayLength,r-1);
-            }
-    }
-
+        while (index > -1){
     
+        System.out.println("  ");    
+        System.out.println("Enter index : ");
+        stringIndex = scanInput.nextLine();
+        index = Integer.parseInt(stringIndex);
+        //System.out.println("Current index = "+ index);
+        
+        if (index > 0 && index < arrayLength+1){
+            printArray(intArray, arrayLength, index-1);
+        }// end if
+        else  {//System.out.println("Program Stoped because you entered "+ index + ",a number that is less than 0...");
+              System.out.print("Index "+ index+ " does not exist, you played your self...");
+        }// end else
+         }// end while
+        System.out.print("program ended");
+          }// end main
+
     public static void printArray(int[] array, int length, int i){
         System.out.println((("Array"+(i+1) + " = " + array[i])));
-    }
-    }
+    }// end printArray
+     }// end class
 
